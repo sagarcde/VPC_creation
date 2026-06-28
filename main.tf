b0019424d14a79f0be27cd68789fc66b0ed7ca12
sagarcde <sagar.cde999@gmail.com>
2026-06-28 20:38:30 +0530

    terraform

--- main.tf
+++ main.tf
@@ -136,13 +136,11 @@ resource "aws_route" "public_internet_access" {
   gateway_id = aws_internet_gateway.main.id
 }
 resource "aws_route" "private_nat_access" {
-  count = length(var.private_subnet_cidrs)
   route_table_id = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
 resource "aws_route" "database_nat_access" {
-  count = length(var.database_subnet_cidrs)
   route_table_id = aws_route_table.database.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
