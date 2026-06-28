7e187259581193ee2a7e68fe19f02d806a00864d
sagarcde <sagar.cde999@gmail.com>
2026-06-28 20:31:44 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -19,24 +19,28 @@ resource "aws_vpc_peering_connection" "peering" {
 }
 
 resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
   route_table_id = aws_route_table.public.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
   
  }
  resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
   route_table_id = aws_route_table.private.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
  }
  resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
   route_table_id = aws_route_table.database.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
  }
 
 resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
   route_table_id = data.aws_vpc.default.main_route_table_id
   destination_cidr_block = aws_vpc.main.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
